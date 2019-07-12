3fa63252cc2bd4b2e3c1c37a8d971fa01b0bb20d
Aman Sharma <sharmaman4419@gmail.com>
2019-07-12 08:17:04 +0530

    complete exercise

--- assignments/1. hello-world/practice/index.js
+++ assignments/1. hello-world/practice/index.js
@@ -1,18 +1,31 @@
 // Challenge 1.
 // Log message in console saying "I'm Learning Javascript"
-
+//
+console.log("im learning javaScript");
+//
 // Challenge 2.
 // Declare two variables: admin and name.
 // Assign the value "John" to name.
 // Copy the value from name to admin.
 // Show the value of admin using alert (must output “John”).
-
+//
+let admin;
+let name;
+name = "john";
+admin = name;
+alert(admin);
+//
 // Challenge 3.
 // Use alert function to to show a message saying “Hello World”.
-
+//
+alert("hello -world")
+//
 // Challenge 4.
 // Use prompt to take the name of person and greets them. i.e "Welcome Arun"
-
+//
+prompt("enter name");
+alert("welcome arun");
+//
 // Challenge 5.
 // Get an input from the user using the prompt box and display the length of the string.
 // e.g “Learning is fun” should output will be 15.
